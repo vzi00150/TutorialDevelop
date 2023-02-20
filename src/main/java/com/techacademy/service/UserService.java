4102b914959822a3451df71f7cd43ba2b3381483
vzi00150 <vzi00150@gmail.com>
2023-02-20 20:11:58 +0900

    chapter7まで終了

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -1,7 +1,10 @@
 package com.techacademy.service;
 
 import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
 
@@ -17,5 +20,11 @@ public class UserService {
     public List<User> getUserList() {
         // リポジトリのfindAllメソッドを呼び出す
         return userRepository.findAll();
+    }
+
+    /** Userの登録を行う */
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
     }
 }
